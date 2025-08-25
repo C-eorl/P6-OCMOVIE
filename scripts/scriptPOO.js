d03a12d5e044533f8f7c2b344f49386048468159
Ceorl <dev.rocher.florian@gmail.com>
2025-08-25 16:06:15 +0200

    suppression fonction utilitaire

--- scripts/scriptPOO.js
+++ scripts/scriptPOO.js
@@ -1,32 +1,5 @@
-import { Movie, Section, createMovies, BestSection } from "./models.js";
-
-/** Créez de faux film pour initialisation des Section Autre */
-function getEmptyMovies(count = 6) {
-    const emptyMovies = [];
-    for (let i = 0; i < count; i++) {
-        emptyMovies.push(new Movie(
-            "",              // url
-            "Film indisponible", // titre par défaut
-            "https://via.placeholder.com/150?text=No+Image", // image par défaut
-            "Aucune description disponible." // description par défaut
-        ));
-    }
-    return emptyMovies;
-}
-
-
-/** récupère la liste complète des categorie de l'API et la retourne */
-async function getCategories() {
-    const response = await fetch("http://127.0.0.1:8000/api/v1/genres/?page_size=25");
-    const data = await response.json();
-    let categories = []
-    data["results"].forEach(category => {
-        categories.push(category["name"])
-    });
-
-    return categories
-
-}
+import { Movie, Section, BestSection } from "./models.js";
+import { getEmptyMovies, createMovies, getCategories, } from "./utils.js";
 
 /** Demande les données des films trié par score IMDB
  *  Récupère le premier
